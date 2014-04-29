02d784372215e573effe9c16ef2a41bd6b96220e
kerbymart <kerbymart@gmail.com>
2014-04-29 18:22:53 +0800

    Update type

---
+++ src/main/java/org/appobjects/types/Update.java
@@ -0,0 +1,12 @@
+package org.appobjects.types;
+
+import java.util.Iterator;
+
+/**
+ * Created by kerby on 4/29/14.
+ */
+public class Update {
+    public <V> Iterator<V> now() {
+        throw new RuntimeException("Not yet implemented");
+    }
+}
