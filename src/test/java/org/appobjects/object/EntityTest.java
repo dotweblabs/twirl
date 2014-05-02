5d07b6d1fc4ce2415d6a1e635319562c238c47cc
kerbymart <kerbymart@gmail.com>
2014-05-02 19:25:41 +0800

    Add simple Entity and Datastore test

---
+++ src/test/java/org/appobjects/object/EntityTest.java
@@ -0,0 +1,27 @@
+package org.appobjects.object;
+
+import com.google.appengine.api.datastore.*;
+import org.appobjects.LocalDatastoreTestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by kerby on 5/2/14.
+ */
+public class EntityTest extends LocalDatastoreTestCase {
+
+    DatastoreService _ds = DatastoreServiceFactory.getDatastoreService();
+
+    @Test
+    public void test(){
+        Entity e = new Entity(KeyFactory.createKey("TestKind", "TestKey"));
+        Key key = _ds.put(e);
+        try {
+            Entity test = _ds.get(key);
+            assertNotNull(test);
+        } catch (EntityNotFoundException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
