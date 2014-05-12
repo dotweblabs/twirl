c2bc31314e51825c1363211f54b2b02bd2aa1020
kerbymart <kerbymart@gmail.com>
2014-05-12 12:53:20 +0800

    Use Guava import

--- src/test/java/org/appobjects/object/ObjectStoreTest.java
+++ src/test/java/org/appobjects/object/ObjectStoreTest.java
@@ -3,7 +3,7 @@ package org.appobjects.object;
 import static org.boon.Lists.lists;
 import static org.junit.Assert.*;
 
-import com.google.appengine.labs.repackaged.com.google.common.collect.Lists;
+import com.google.common.collect.Lists;
 import org.appobjects.GaeObjectStore;
 import com.google.appengine.api.datastore.Key;
 import org.appobjects.ObjectStore;
